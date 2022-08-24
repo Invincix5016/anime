67d966c2b0515185cd7ef28f227ff37322099307
Julian Garnier <julian.garnier@gmail.com>
2022-08-24 12:16:49 +0200

    Update .remove() to use tweens instead of animations

--- tests/animatables.test.js
+++ tests/animatables.test.js
@@ -6,7 +6,7 @@ describe('Animatables', () => {
     });
 
     const targetEl = document.querySelector('#target-id');
-    expect(animation.animations.length).toBe(1);
+    expect(animation.tweens.length).toBe(1);
     expect(animation.targets[0]).toBe(targetEl);
   });
 
@@ -17,7 +17,7 @@ describe('Animatables', () => {
     });
 
     const targetEls = document.querySelectorAll('.target-class');
-    expect(animation.animations.length).toBe(4);
+    expect(animation.tweens.length).toBe(4);
     targetEls.forEach((el, i) => {
       expect(animation.targets[i]).toBe(el);
     });
@@ -30,7 +30,7 @@ describe('Animatables', () => {
       translateX: 100
     });
 
-    expect(animation.animations.length).toBe(1);
+    expect(animation.tweens.length).toBe(1);
     expect(animation.targets[0]).toBe(targetEl);
   });
 
@@ -41,7 +41,7 @@ describe('Animatables', () => {
       translateX: 100
     });
 
-    expect(animation.animations.length).toBe(4);
+    expect(animation.tweens.length).toBe(4);
     targetEls.forEach((el, i) => {
       expect(animation.targets[i]).toBe(el);
     });
@@ -53,7 +53,7 @@ describe('Animatables', () => {
       plainValue: 200
     });
 
-    expect(animation.animations.length).toBe(1);
+    expect(animation.tweens.length).toBe(1);
     expect(animation.targets[0]).toBe(testObject);
   });
 
@@ -66,7 +66,7 @@ describe('Animatables', () => {
     const targetIdEl = document.querySelector('#target-id');
     const targetClassEls = document.querySelectorAll('.target-class');
     const targetDataEl = document.querySelector('div[data-index="0"]');
-    expect(animation.animations.length).toBe(4);
+    expect(animation.tweens.length).toBe(4);
     expect(animation.targets[0]).toBe(targetIdEl);
     expect(animation.targets[0]).toBe(targetDataEl);
     targetClassEls.forEach((el, i) => {
@@ -83,7 +83,7 @@ describe('Animatables', () => {
 
     const targetIdEl = document.querySelector('#target-id');
     const targetDataEl = document.querySelector('div[data-index="0"]');
-    expect(animation.animations.length).toBe(5);
+    expect(animation.tweens.length).toBe(5);
     expect(animation.targets[0]).toBe(testObject);
     expect(animation.targets[1]).toBe(targetIdEl);
     expect(animation.targets[1]).toBe(targetDataEl);
@@ -96,7 +96,7 @@ describe('Animatables', () => {
     });
     animation.seek(animation.duration);
     expect(animation.targets.length).toBe(0);
-    expect(animation.animations.length).toBe(0);
+    expect(animation.tweens.length).toBe(0);
     expect(animation.duration).toBe(100);
   });
 
@@ -105,13 +105,13 @@ describe('Animatables', () => {
       targets: [testObject, anOtherTestObject],
       plainValue: 200,
     });
-    expect(animation.animations.length).toBe(2);
+    expect(animation.tweens.length).toBe(2);
 
     anime.remove(testObject);
-    expect(animation.animations.length).toBe(1);
+    expect(animation.tweens.length).toBe(1);
 
     anime.remove(anOtherTestObject);
-    expect(animation.animations.length).toBe(0);
+    expect(animation.tweens.length).toBe(0);
   });
 
   test('Remove targets from multiple instances at once', () => {
@@ -123,16 +123,16 @@ describe('Animatables', () => {
       targets: anOtherTestObject,
       plainValue: 300,
     });
-    expect(animation1.animations.length).toBe(2);
-    expect(animation2.animations.length).toBe(1);
+    expect(animation1.tweens.length).toBe(2);
+    expect(animation2.tweens.length).toBe(1);
 
     anime.remove(testObject);
-    expect(animation1.animations.length).toBe(1);
-    expect(animation2.animations.length).toBe(1);
+    expect(animation1.tweens.length).toBe(1);
+    expect(animation2.tweens.length).toBe(1);
 
     anime.remove(anOtherTestObject);
-    expect(animation1.animations.length).toBe(0);
-    expect(animation2.animations.length).toBe(0);
+    expect(animation1.tweens.length).toBe(0);
+    expect(animation2.tweens.length).toBe(0);
   });
 
   test('Remove targets on a specific instance', () => {
@@ -144,24 +144,24 @@ describe('Animatables', () => {
       targets: [anOtherTestObject, testObject],
       plainValue: 300,
     });
-    expect(animation1.animations.length).toBe(2);
-    expect(animation2.animations.length).toBe(2);
+    expect(animation1.tweens.length).toBe(2);
+    expect(animation2.tweens.length).toBe(2);
 
     animation1.remove(anOtherTestObject);
-    expect(animation1.animations.length).toBe(1);
-    expect(animation2.animations.length).toBe(2);
+    expect(animation1.tweens.length).toBe(1);
+    expect(animation2.tweens.length).toBe(2);
 
     animation2.remove(testObject);
-    expect(animation1.animations.length).toBe(1);
-    expect(animation2.animations.length).toBe(1);
+    expect(animation1.tweens.length).toBe(1);
+    expect(animation2.tweens.length).toBe(1);
 
     animation1.remove(testObject);
-    expect(animation1.animations.length).toBe(0);
-    expect(animation2.animations.length).toBe(1);
+    expect(animation1.tweens.length).toBe(0);
+    expect(animation2.tweens.length).toBe(1);
 
     animation2.remove(anOtherTestObject);
-    expect(animation1.animations.length).toBe(0);
-    expect(animation2.animations.length).toBe(0);
+    expect(animation1.tweens.length).toBe(0);
+    expect(animation2.tweens.length).toBe(0);
   });
 
   test('Remove targets with CSS selectors', () => {
@@ -169,16 +169,16 @@ describe('Animatables', () => {
       targets: ['#target-id', '.target-class', 'div[data-index="0"]'],
       translateX: 100,
     });
-    expect(animation.animations.length).toBe(4);
+    expect(animation.tweens.length).toBe(4);
 
     anime.remove('#target-id');
-    expect(animation.animations.length).toBe(3);
+    expect(animation.tweens.length).toBe(3);
 
     anime.remove('[data-index="2"]');
-    expect(animation.animations.length).toBe(2);
+    expect(animation.tweens.length).toBe(2);
 
     anime.remove('.target-class');
-    expect(animation.animations.length).toBe(0);
+    expect(animation.tweens.length).toBe(0);
   });
 
   test('Do not pause instances with no targets when calling remove', resolve => {
@@ -193,9 +193,9 @@ describe('Animatables', () => {
       targets: '#target-id',
       translateX: 100,
     });
-    expect(animation.animations.length).toBe(1);
+    expect(animation.tweens.length).toBe(1);
 
     anime.remove('#target-id');
-    expect(animation.animations.length).toBe(0);
+    expect(animation.tweens.length).toBe(0);
   });
 });
